3d40e11d443344d9ed5a2eb6bb30ecc09d58bf08
Leonardo22sp <santos.leonardo22sp@gmail.com>
2025-09-04 19:22:37 -0300

    modificacao do arquivo views.py

--- receitas/views.py
+++ receitas/views.py
@@ -2,29 +2,26 @@ from django.shortcuts import render, get_object_or_404
 from .models import Receita
 
 def home(request):
-    return render(request, 'receitas/home.html')
-
-def pesquisar_receitas(request):
-    return render(request, 'receitas/home.html')
+    receitas = Receita.objects.all()
+    return render(request, 'receitas/home.html', {'receitas': receitas})
 
 def receita_detail(request, id):
     receita = get_object_or_404(Receita, pk=id)
+    return render(request, 'receitas/receita_detail.html', {'receita': receita})
 
+def pesquisar_receitas(request):
+    query = request.GET.get('q')
+    resultados = []
+    if query:
+        resultados = Receita.objects.filter(title__icontains=query)
+    return render(request, 'receitas/pesquisa.html', {
+        'query': query,
+        'receitas': resultados
+    })
     context = {
         'receita': receita,
     }
 
     return render(request, 'receitas/receita_detail.html', context)
 
-def pesquisar_receitas(request):
-    query = request.GET.get('q')
-    resultados = []
 
-    if query:
-        resultados = Receita.objects.filter(title__icontains=query)
-
-    context = {
-        'query': query,
-        'resultados': resultados,
-    }
-    return render(request, 'receitas/pesquisar_receitas.html', context)
